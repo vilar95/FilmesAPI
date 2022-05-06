93c395e6dbf6f72b25a6e9a8543e8d782b0b07d7
EDUARDO VILAR <e.vilar95@gmail.com>
2022-05-06 14:14:43 -0300

    HttpPost

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -12,7 +12,7 @@ namespace FilmesAPI.Controllers
         private static List<Filme> filmes = new List<Filme>();
         
         [HttpPost]
-        public void AdicionarFilme(Filme filme)
+        public void AdicionarFilme([FromBody]Filme filme)
         {
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
